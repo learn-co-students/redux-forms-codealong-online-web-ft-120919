35d91a7c79cdb2edf82365a33663c0d9354791ae
mrarthurwhite <mister.arthur.white@gmail.com>
2020-05-26 08:05:31 -0400

    Done.

---
+++ src/components/todos/CreateTodov3.js
@@ -0,0 +1,37 @@
+// ./src/components/todos/CreateTodo.js
+import React, { Component } from 'react';
+
+class CreateTodo extends Component {
+
+    constructor() {
+        super();
+        this.state = {
+            text: '',
+        };
+    }
+
+    handleChange = event => {
+        this.setState({
+            text: event.target.value
+        });
+    }
+
+    render() {
+        return(
+            <div>
+                <form>
+                    <p>
+                        <label>add todo</label>
+                        <input
+                            type="text"
+                            onChange={this.handleChange} value={this.state.text}/>
+                    </p>
+                    <input type="submit" />
+                </form>
+                {this.state.text}
+            </div>
+        );
+    }
+};
+
+export default CreateTodo;
